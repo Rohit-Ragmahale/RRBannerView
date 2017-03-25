6939b9d63ae10fe57d6e2b9cb9f2cd65b6f35349
Rohit Ragmahale <120hit@rohit.local>
2017-03-25 21:14:45 +0530

    UI changes

--- Demo/RRBannerView/ViewController.swift
+++ Demo/RRBannerView/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBAction func showBannerViewAtTop(sender: AnyObject) {
-        let bannner: RRBannerView = RRBannerView(title: "RRBannerView", message: "", image:UIImage(named: "like"), view: self.view)
+        let bannner: RRBannerView = RRBannerView(title: "", message: "", image:UIImage(named: "like"), view: self.view)
         bannner.autoDismiss = true
         bannner.show(.top)
     }
     
     @IBAction func showBannerViewAtBottom(sender: AnyObject) {
-        let bannner: RRBannerView = RRBannerView(title: "RRBannerView", message: "Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved.", image:UIImage(named: "like"), view: self.view)
-        bannner.show(.middle)
+        let bannner: RRBannerView = RRBannerView(title: "RRBannerView", message: "iOS is the second most popular mobile operating system in the world, after Android.", image:UIImage(named: "like"), view: self.view)
+        bannner.autoDismiss = true
+        bannner.show(.bottom)
     }
     
     @IBAction func showBannerViewAtMiddle(sender: AnyObject) {
-        let bannner: RRBannerView = RRBannerView(title: "RRBannerView", message: "Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved. Created by Rohit Ragmahale on 19/03/17. Copyright © 2017 Rohit Ragmahale. All rights reserved.", image:UIImage(named: "like"), view: self.view)
-        bannner.show(.bottom)
+        let bannner: RRBannerView = RRBannerView(title: "iOS", message: "iOS (formerly iPhone OS) is a mobile operating system created and developed by Apple Inc. exclusively for its hardware. It is the operating system that presently powers many of the company's mobile devices, including the iPhone, iPad, and iPod Touch. It is the second most popular mobile operating system globally after Android. iPad tablets are also the second most popular, by sales, against Android since 2013", image:UIImage(named: "like"), view: self.view)
+        bannner.show(.middle)
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RRBannerView
 Simple banner view that can be used to show short messages and alerts in iOS apps
+
+## Features
+* Simple view having title, message and image
+* Easy to use
+* Tap dismiss and auto dismiss feature
+* Banner can be showned at top / middle / bottom of container view
